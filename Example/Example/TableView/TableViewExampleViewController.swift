d0ea5f3ca5025d0d8ac4127289bcd631d8485a07
MinhHien Vo <minhhien177@gmail.com>
2018-10-01 01:03:47 +0800

    Refactor Example App

--- Example/Example/TableView/TableViewExampleViewController.swift
+++ Example/Example/TableView/TableViewExampleViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TableViewExampleViewController.swift
 //  Example
 //
 //  Created by Hien Vo Minh on 30/9/18.
@@ -10,7 +10,7 @@ import UIKit
 import CleanUIKit
 import SnapKit
 
-final class ViewController: UIViewController {
+final class TableViewExampleViewController: UIViewController {
 
   private lazy var tableView: UITableView = create(superview: view, target: self) { tableView in
     tableView.reuse.registerAll(TitleTableViewCell.self,
@@ -27,7 +27,7 @@ final class ViewController: UIViewController {
 
 }
 
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
+extension TableViewExampleViewController: UITableViewDataSource, UITableViewDelegate {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return 10
   }
