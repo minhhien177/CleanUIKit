e0c90ac495f82815075cb42bfe966bde51cfeb8e
MinhHien Vo <minhhien177@gmail.com>
2018-09-30 22:14:41 +0800

    Initial Project

---
+++ CleanUIKit/CleanUIKit.h
@@ -0,0 +1,19 @@
+//
+//  CleanUIKit.h
+//  CleanUIKit
+//
+//  Created by Hien Vo Minh on 30/9/18.
+//  Copyright © 2018 vomh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CleanUIKit.
+FOUNDATION_EXPORT double CleanUIKitVersionNumber;
+
+//! Project version string for CleanUIKit.
+FOUNDATION_EXPORT const unsigned char CleanUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanUIKit/PublicHeader.h>
+
+
